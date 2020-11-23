8441071bd2306d6d91a8ccd7c02d2470684a49c1
Emir <dzhetybaev19emir@gmail.com>
2020-11-23 15:07:00 +0600

    init project

---
+++ precache-manifest.f13f8827d013c502b08e1c0f5bebf764.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00e6273b0f2ae078b95",
+    "url": "/resume/css/app.a99cf8f8.css"
+  },
+  {
+    "revision": "adadacdc26fa80c71d5e",
+    "url": "/resume/css/chunk-vendors.c5b0137c.css"
+  },
+  {
+    "revision": "b657253cd7e0a031d78478701828b3ea",
+    "url": "/resume/img/client-form.png"
+  },
+  {
+    "revision": "3ea1819e62f0f6cae82753e27ee91bbc",
+    "url": "/resume/img/coworking.png"
+  },
+  {
+    "revision": "c39ca752f9abaa9f0cfa284fee37d7e3",
+    "url": "/resume/img/fastfood.png"
+  },
+  {
+    "revision": "0dcfa4b8edc28a10ff9c0d6bfe0b0ade",
+    "url": "/resume/img/movies.png"
+  },
+  {
+    "revision": "3c1c20fb71d8fb142624365cd4a04054",
+    "url": "/resume/img/nature.jpg"
+  },
+  {
+    "revision": "34341cf58859ea79cf59d1f593bda0e4",
+    "url": "/resume/img/photo.jpeg"
+  },
+  {
+    "revision": "018f774e6d232f88195da840367ac56d",
+    "url": "/resume/img/private-account.png"
+  },
+  {
+    "revision": "63378985a066b7f9a39fe83c54a17e4e",
+    "url": "/resume/img/restaurant.png"
+  },
+  {
+    "revision": "21f8f47cbdfe690366bf9c3447a92a88",
+    "url": "/resume/img/simon-game.png"
+  },
+  {
+    "revision": "78aced311d387a56dec2b90000bf4744",
+    "url": "/resume/img/todo-list.png"
+  },
+  {
+    "revision": "aadf3b3a7dcddea1e6360cae36c5252c",
+    "url": "/resume/img/weather.png"
+  },
+  {
+    "revision": "cfe35ef2928ddbb84a93f3ec82f8c328",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "e00e6273b0f2ae078b95",
+    "url": "/resume/js/app.2674d2eb.js"
+  },
+  {
+    "revision": "adadacdc26fa80c71d5e",
+    "url": "/resume/js/chunk-vendors.18db8883.js"
+  },
+  {
+    "revision": "ac6431a78f80411175aeb01f42ee0be9",
+    "url": "/resume/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/resume/robots.txt"
+  },
+  {
+    "revision": "ec2d5a1c7cac4cc31148a36a9ebf9cff",
+    "url": "/resume/style.css"
+  }
+]);
